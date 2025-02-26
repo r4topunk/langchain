355b70af888085fac01b6af92820fd4a74661055
r4topunk <gustavo-kuhl@hotmail.com>
2025-02-26 11:26:13 -0300

    feat: add LangChain dependencies and implement translation model

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import { ChatOpenAI } from "@langchain/openai";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+
+const model = new ChatOpenAI({ model: "gpt-4" });
+
+const messages = [
+  new SystemMessage(
+    "Translate the following from English into Brazilian Portuguese"
+  ),
+  new HumanMessage("hi!"),
+];
+
+const invoketion = await model.invoke(messages);
+console.log(invoketion);
