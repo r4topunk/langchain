512519e27a9b1253c8042c1601e807d9c945ae97
r4topunk <gustavo-kuhl@hotmail.com>
2025-03-01 10:01:26 -0300

    feat: text classification 1

---
+++ text-classification.ts
@@ -0,0 +1,40 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { z } from "zod";
+
+const llm = new ChatOpenAI({
+  model: "gpt-4o-mini",
+  temperature: 0,
+});
+
+const taggingPrompt = ChatPromptTemplate.fromTemplate(
+  `Extract the desired information from the following passage.
+  
+Only extract the properties mentioned in the 'Classification' function.
+
+Passage:
+{input}
+`
+);
+
+const classificationSchema = z.object({
+  sentiment: z.string().describe("The sentiment of the text"),
+  aggressiveness: z
+    .number()
+    .int()
+    // .min(1) // do not work on openai model
+    // .max(10) // do not work on openai model
+    .describe("How aggressive the text is on a scale from 1 to 10"),
+  language: z.string().describe("The language the text is written in"),
+});
+
+// Name is optional, but gives the models more clues as to what your schema represents
+const llmWithStructuredOutput = llm.withStructuredOutput(classificationSchema, {
+  name: "extractor",
+});
+
+const prompt1 = await taggingPrompt.invoke({
+  input: "Porra assim não da mano, sem condições uma merda dessas",
+});
+const output1 = await llmWithStructuredOutput.invoke(prompt1);
+console.log(output1);
