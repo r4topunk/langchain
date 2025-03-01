dbd2af687aa92826dd3c784a806a8024dc7b8990
r4topunk <gustavo-kuhl@hotmail.com>
2025-03-01 16:05:26 -0300

    feat: text classification 2

--- text-classification.ts
+++ text-classification.ts
@@ -9,7 +9,7 @@ const llm = new ChatGroq({
 
 const taggingPrompt = ChatPromptTemplate.fromTemplate(
   `Extract the desired information from the following passage.
-  
+
 Only extract the properties mentioned in the 'Classification' function.
 
 Passage:
@@ -17,7 +17,7 @@ Passage:
 `
 );
 
-const classificationSchema = z.object({
+const classificationSchema1 = z.object({
   sentiment: z.string().describe("The sentiment of the text"),
   aggressiveness: z
     .number()
@@ -29,12 +29,54 @@ const classificationSchema = z.object({
 });
 
 // Name is optional, but gives the models more clues as to what your schema represents
-const llmWithStructuredOutput = llm.withStructuredOutput(classificationSchema, {
-  name: "extractor",
-});
+const llmWithStructuredOutput1 = llm.withStructuredOutput(
+  classificationSchema1,
+  {
+    name: "extractor",
+  }
+);
 
 const prompt1 = await taggingPrompt.invoke({
   input: "Porra assim não da mano, sem condições uma merda dessas",
 });
-const output1 = await llmWithStructuredOutput.invoke(prompt1);
-console.log(output1);
+const output1 = await llmWithStructuredOutput1.invoke(prompt1);
+console.log("output1", output1);
+
+const classificationSchema2 = z.object({
+  sentiment: z.enum(["happy", "neutral", "sad"]),
+  aggressivenes: z
+    .number()
+    .int()
+    .min(1)
+    .max(5)
+    .describe(
+      "Describes how aggressive the statement is, the higher the number the more aggressive"
+    ),
+  language: z
+    .enum(["spanish", "english", "french", "german", "italian", "portuguese"])
+    .describe("The language the text is written in"),
+});
+
+const llmWithStructuredOutput2 = llm.withStructuredOutput(
+  classificationSchema2,
+  { name: "extractor" }
+);
+
+const prompt2 = await taggingPrompt.invoke({
+  input:
+    "Estoy increiblemente contento de haberte conocido! Creo que seremos muy buenos amigos!",
+});
+const result2 = await llmWithStructuredOutput2.invoke(prompt2);
+console.log("result2", result2);
+
+const prompt3 = await taggingPrompt.invoke({
+  input: "Estoy muy enojado con vos! Te voy a dar tu merecido!",
+});
+const result3 = await llmWithStructuredOutput2.invoke(prompt3);
+console.log("result3", result3);
+
+const prompt4 = await taggingPrompt.invoke({
+  input: "Weather is ok here, I can go outside without much more than a coat",
+});
+const result4 = await llmWithStructuredOutput2.invoke(prompt4);
+console.log("result4", result4);
